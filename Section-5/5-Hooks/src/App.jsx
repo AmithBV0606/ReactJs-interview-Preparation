a46f989635f6ecd66bace92235ba79f393dc1326
AmithBV0606 <amithrao0606@gmail.com>
2024-08-12 08:37:47 +0530

    Initialized a vite react project.

---
+++ Section-5/5-Hooks/src/App.jsx
@@ -0,0 +1,12 @@
+import UseState from "./components/UseState"
+
+function App() {
+  return (
+    <div>
+      <h2>Hooks Interview Questions in React</h2>
+      <UseState />
+    </div>
+  )
+}
+
+export default App;
